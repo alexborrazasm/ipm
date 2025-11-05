95ab6d019b8aa46c19304610bc64d7d90b0fb4ac
alexborrazasm <alexborrazasm@gmail.com>
2025-11-05 17:36:44 +0100

    [View] Añadido botón de añadir gasto

--- splitwiththemachine/lib/ui/features/expenses/view/expenses_list_view.dart
+++ splitwiththemachine/lib/ui/features/expenses/view/expenses_list_view.dart
@@ -66,7 +66,11 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
           );
         },
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {}, // TODO
+        tooltip: 'Add an expense',
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
