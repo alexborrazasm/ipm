9d62e7e3d59f7e6852b645b785d08cdb560cbc31
NereaPP <nereaperezpertega@gmail.com>
2025-11-05 12:30:32 +0100

    [MAIN] corregido main

--- splitwiththemachine/lib/main.dart
+++ splitwiththemachine/lib/main.dart
@@ -7,43 +7,43 @@ import 'ui/expenses_view.dart';
 import 'ui/expenses_viewmodel.dart';
 
 void main() {
-  final apiService = SplitWithMeAPIService(); // Only one instance
-
-  final providers = [
-    Provider(
-      create: (context) => FriendRepository(service: apiService),
-    ),
-    Provider(
-      create: (context) => ExpenseRepository(service: apiService),
-    ),
-  ];
-
-  runApp(
-    MultiProvider(
-      providers: providers,
-      child: const MyApp(),
-    ),
+  runApp(MyApp(
+      expenseRepository: ExpenseRepository(service: SplitWithMeAPIService()),
+      friendRepository: FriendRepository(service: SplitWithMeAPIService())
+        )
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key, required this.expenseRepository,
+    required this.friendRepository});
+
+  final FriendRepository friendRepository;
+  final ExpenseRepository expenseRepository;
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'SplitWithFriends',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-      ),
-      home: FriendsScreen(
-        title: 'Friend list',
-        viewModel: ExpenseViewModel(
-            expenseRepository: context.read(),
-            friendRepository: context.read()
-        ),
-      ),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => expenseRepository),
+        Provider(create: (context) => friendRepository),
+      ],
+      builder: (context, widget) {
+        return MaterialApp(
+          title: 'SplitWithFriends',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+          ),
+          home: FriendsScreen(
+            title: 'Friend list',
+            viewModel: ExpenseViewModel(
+                expenseRepository: context.read(),
+                friendRepository: context.read()
+            ),
+          ),
+        );
+      }
     );
   }
 }
