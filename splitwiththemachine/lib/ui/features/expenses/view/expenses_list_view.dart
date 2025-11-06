7617d29244373d9e0f162afd69103e9e2cbe5bc1
alexborrazasm <alexborrazasm@gmail.com>
2025-11-06 14:12:18 +0100

    [View] Añadido menu a expense list y primer intento de manejo de errores

--- splitwiththemachine/lib/ui/features/expenses/view/expenses_list_view.dart
+++ splitwiththemachine/lib/ui/features/expenses/view/expenses_list_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../viewmodel/expenses_viewmodel.dart';
 import '../widgets/expense_row.dart';
+import 'package:splitwiththemachine/ui/core/widgets/info_bar.dart';
 
 class ExpenseListScreen extends StatefulWidget {
   const ExpenseListScreen({
@@ -23,7 +24,24 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
-        centerTitle: true
+        centerTitle: true,
+        actions: [
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              switch (value) {
+                case 'help_feedback':
+                  Navigator.pushNamed(context, '/help_feedback');
+                  break;
+              }
+            },
+            itemBuilder: (BuildContext context) => [
+              const PopupMenuItem(
+                value: 'help_feedback',
+                child: Text('Help and feedback'),
+              )
+            ],
+          ),
+        ],
       ),
       body: ListenableBuilder(
         listenable: Listenable.merge([
@@ -34,31 +52,49 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
           return Stack(
             children: [
               if (widget.viewModel.loadExpenses.running)
-                Center(child: CircularProgressIndicator()),
+                const Center(child: CircularProgressIndicator()),
 
-              widget.viewModel.expenses.isEmpty
-                  ? Center(child: Text("No expenses"))
-                  : CustomScrollView(
-                slivers: [
-                  SliverPadding(
-                    padding: const EdgeInsets.only(
-                        top: 8, left: 12,
-                        right: 12, bottom: 8
-                    ),
-                    sliver: SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                          return ExpenseRow(
-                            expense: widget.viewModel.expenses[index],
-                          );
-                        },
-                        childCount: widget.viewModel.expenses.length,
+              if (!widget.viewModel.loadExpenses.running)
+                widget.viewModel.expenses.isEmpty
+                    ? const Center(child: Text("No expenses"))
+                    : CustomScrollView(
+                  slivers: [
+                    SliverPadding(
+                      padding: const EdgeInsets.only(
+                        top: 8,
+                        left: 12,
+                        right: 12,
+                        bottom: 8,
+                      ),
+                      sliver: SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                              (context, index) {
+                            return ExpenseRow(
+                              expense: widget.viewModel.expenses[index],
+                            );
+                          },
+                          childCount: widget.viewModel.expenses.length,
+                        ),
                       ),
                     ),
+                    const SliverToBoxAdapter(
+                      child: SizedBox(height: 200),
+                    ),
+                  ],
+                ),
+
+              if (widget.viewModel.loadExpenses.error)
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: InfoBar(
+                    message: widget.viewModel.errorMessage!,
+                    onPressed: () {
+                      widget.viewModel.loadExpenses.clearResult();
+                      widget.viewModel.loadExpenses.execute();
+                    },
+                    isError: true,
                   ),
-                  const SliverToBoxAdapter(child: SizedBox(height: 200)),
-                ],
-              ),
+                ),
             ],
           );
         },
@@ -67,7 +103,7 @@ class _ExpenseListScreenState extends State<ExpenseListScreen> {
         tooltip: 'Add an expense',
         child: const Icon(Icons.add),
           onPressed: () {
-            Navigator.pushNamed(context, '/help_feedback');
+            Navigator.pushNamed(context, '/add_expense');
           },
       )
     );
