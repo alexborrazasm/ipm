79c78d87bf993fcc4fe64fbfaecd7af44a1a59d9
NereaPP <nerea.ppertega@udc.es>
2025-11-06 20:25:16 +0100

    [VIEW] Añadidos tooltips

--- splitwiththemachine/lib/ui/features/help_feedback/view/help_feedback_view.dart
+++ splitwiththemachine/lib/ui/features/help_feedback/view/help_feedback_view.dart
@@ -32,116 +32,121 @@ class HelpAndFeedbackScreen extends StatelessWidget {
                         )),
                     const SizedBox(height: 16.0),
                     Image.asset('assets/images/logo.png', height: 100.0,),
-                    const SizedBox(height: 16.0),
                     const Text('0.1', style: TextStyle(fontSize: 16.0)),
                     const SizedBox(height: 24.0),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 4,
-                        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                      ),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Authors'),
-                              content: const SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text('Alexandre Borrazás Mancebo'),
-                                    Text('Daniel García Figueroa'),
-                                    Text('Nerea Pérez Pértega')
-                                  ],
+                    Tooltip(
+                      message: 'Show authors',
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 4,
+                          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Authors'),
+                                content: const SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text('Alexandre Borrazás Mancebo'),
+                                      Text('Daniel García Figueroa'),
+                                      Text('Nerea Pérez Pértega')
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Close'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: const Text('Authors'),
-                    ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Close'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('Authors'),
+                      ),),
                     const SizedBox(height: 12.0),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 4,
-                        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                      ),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('License'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text('© 2025 Alexandre Borrazás Mancebo, '
-                                        'Daniel García Figueroa and '
-                                        'Nerea Pérez Pértega. '
-                                        'This program comes WITHOUT ANY WARRANTY. '
-                                        'See the GNU General Public License, version'
-                                        '3 or later for more details.'),
-                                  ],
+                    Tooltip(
+                      message: 'Show license',
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 4,
+                          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('License'),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text('© 2025 Alexandre Borrazás Mancebo, '
+                                          'Daniel García Figueroa and '
+                                          'Nerea Pérez Pértega. '
+                                          'This program comes WITHOUT ANY WARRANTY. '
+                                          'See the GNU General Public License, version'
+                                          ' 3 or later for more details.'),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Close'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: const Text('License'),
-                    ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Close'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('License'),
+                      ),),
                     const SizedBox(height: 12.0),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        elevation: 4,
-                        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                      ),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: const Text('Send us feedback!'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text('alexandre.bmancebo@udc.es'),
-                                    Text('d.figueroa@udc.es'),
-                                    Text('nerea.ppertega@udc.es'),
-                                  ],
+                    Tooltip(
+                      message: 'Show contact emails',
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          elevation: 4,
+                          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+                        ),
+                        onPressed: () {
+                          showDialog(
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Send us feedback!'),
+                                content: SingleChildScrollView(
+                                  child: ListBody(
+                                    children: <Widget>[
+                                      Text('alexandre.bmancebo@udc.es'),
+                                      Text('d.figueroa@udc.es'),
+                                      Text('nerea.ppertega@udc.es'),
+                                    ],
+                                  ),
                                 ),
-                              ),
-                              actions: <Widget>[
-                                TextButton(
-                                  child: const Text('Close'),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: const Text('Contact us'),
-                    ),
+                                actions: <Widget>[
+                                  TextButton(
+                                    child: const Text('Close'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        },
+                        child: const Text('Contact us'),
+                      ),)
                   ]),
             ),
           ),
