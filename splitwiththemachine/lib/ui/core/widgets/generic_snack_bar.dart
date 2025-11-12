b4cd574d5363248119434fea8d897c149dd9cbe9
NereaPP <nerea.ppertega@udc.es>
2025-11-12 18:51:47 +0100

    [VIEW] Cambiado color snackBar

--- splitwiththemachine/lib/ui/core/widgets/generic_snack_bar.dart
+++ splitwiththemachine/lib/ui/core/widgets/generic_snack_bar.dart
@@ -6,10 +6,26 @@ class GenericSnackBar {
   static void show(BuildContext context, String message) {
     ScaffoldMessenger.of(context).hideCurrentSnackBar();
 
+    // Check if dark or light mode
+    final brightness = Theme.of(context).brightness;
+    final bool isDarkMode = brightness == Brightness.dark;
+
+    // Define colors
+    final Color backgroundColor = isDarkMode
+        ? Colors.grey[800]!
+        : Colors.grey[700]!;
+
+    final Color textColor = isDarkMode
+        ? Colors.white
+        : Colors.white;
+
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
-        content: Text(message),
-        backgroundColor: Theme.of(context).colorScheme.onSurfaceVariant,
+        content: Text(
+            message,
+            style: TextStyle(color: textColor)
+        ),
+        backgroundColor: backgroundColor,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10.0),
         ),
