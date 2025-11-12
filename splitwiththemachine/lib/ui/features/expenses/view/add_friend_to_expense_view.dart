07ad20c86f9ae2a467ed178b63f3b1e03c63cd2b
gitdfigueroa <d.figueroa@udc.es>
2025-11-12 12:27:52 +0100

    Añadido generic_search al add_friend_to_expense

--- splitwiththemachine/lib/ui/features/expenses/view/add_friend_to_expense_view.dart
+++ splitwiththemachine/lib/ui/features/expenses/view/add_friend_to_expense_view.dart
@@ -1,13 +1,12 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:splitwiththemachine/data/models.dart';
 import 'package:splitwiththemachine/ui/core/widgets/centered_message.dart';
 import 'package:splitwiththemachine/ui/core/widgets/scrollable_sliver_list.dart';
 import 'package:splitwiththemachine/ui/core/widgets/generic_app_bar.dart';
 import '../viewmodel/expenses_viewmodel.dart';
+import '../widgets/generic_search.dart';
 
-class AddFriendToExpenseScreen extends StatelessWidget {
+class AddFriendToExpenseScreen extends StatefulWidget {
   const AddFriendToExpenseScreen({
     super.key,
     required this.viewModel,
@@ -18,45 +17,61 @@ class AddFriendToExpenseScreen extends StatelessWidget {
   final Expense expense;
 
   @override
-  Widget build(BuildContext context) {
-    final availableFriends = viewModel.friends
-        .where((f) => !expense.friends.any((e) => e.id == f.id))
-        .toList();
+  State<AddFriendToExpenseScreen> createState() => _AddFriendToExpenseScreenState();
+}
 
+class _AddFriendToExpenseScreenState extends State<AddFriendToExpenseScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: GenericAppBar(
-        title: 'Add Friend',
-      ),
-      body: availableFriends.isEmpty ?
-      const Padding(
-        padding: EdgeInsets.all(32.0),
-        child: CenteredMessage(message: "No friends"),
-      ) : ScrollableSliverList(
-          itemCount: availableFriends.length,
-          itemBuilder: (context, index) {
-            final friend = availableFriends[index];
-            return Card(
-              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              child: InkWell(
-                borderRadius: BorderRadius.circular(12),
-                onTap: () {
-                  Navigator.pop(context);
-                  FriendExpenseArgs args = FriendExpenseArgs(
-                    expense: expense,
-                    friend: friend,
-                  );
-                  viewModel.addFriendToExpense.execute(args);
-                },
-                child: ListTile(
-                  title: Text(friend.name),
-                  leading: const CircleAvatar(
-                    child: Icon(Icons.person),
+      appBar: GenericAppBar(title: 'Add Friend'),
+      body: ListenableBuilder(
+        listenable: widget.viewModel,
+        builder: (context, child) {
+          final availableFriends = widget.viewModel.friends
+              .where((f) => !widget.expense.friends.any((e) => e.id == f.id))
+              .where((f) => GenericSearch.matchSearch(f.name, widget.viewModel.searchQuery))
+              .toList();
+
+          return availableFriends.isEmpty
+              ? const Padding(
+            padding: EdgeInsets.all(32.0),
+            child: CenteredMessage(message: "No friends"),
+          )
+              : ScrollableSliverList(
+            header: GenericSearch(
+              viewModel: widget.viewModel,
+              getSearchQuery: (vm) => vm.searchQuery,
+              onSearchChanged: (vm, value) => vm.search(value),
+              hintText: 'Search friends',
+            ),
+            itemCount: availableFriends.length,
+            itemBuilder: (context, index) {
+              final friend = availableFriends[index];
+              return Card(
+                margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                child: InkWell(
+                  borderRadius: BorderRadius.circular(12),
+                  onTap: () {
+                    Navigator.pop(context);
+                    final args = FriendExpenseArgs(
+                      expense: widget.expense,
+                      friend: friend,
+                    );
+                    widget.viewModel.addFriendToExpense.execute(args);
+                  },
+                  child: ListTile(
+                    title: Text(friend.name),
+                    leading: const CircleAvatar(
+                      child: Icon(Icons.person),
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
-      )
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
