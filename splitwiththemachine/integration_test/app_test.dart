101f8647d3f7bfc7720c1410f25636ded47fc04c
NereaPP <nerea.ppertega@udc.es>
2025-11-21 14:14:04 +0100

    [TESTS] Eliminados imports no usados

--- splitwiththemachine/integration_test/app_test.dart
+++ splitwiththemachine/integration_test/app_test.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -7,12 +5,10 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
 import 'package:splitwiththemachine/main.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:mockito/mockito.dart';
 import 'package:mockito/annotations.dart';
 
 import 'package:splitwiththemachine/data/repositories.dart';
 import 'package:splitwiththemachine/data/services.dart';
-import 'package:splitwiththemachine/data/models.dart';
 
 import 'mock_setup.dart';
 
