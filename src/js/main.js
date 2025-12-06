c1ab2a04d150cd323357e0efccb75794d402e8ea
alexborrazasm <alexborrazasm@gmail.com>
2025-12-06 02:09:57 +0100

    Añadido caché inicial de todos los amigos

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,23 @@
 import * as model from "./model.js";
 import * as ui from "./dom.js";
-import * as cache from "./expensesCache.js";
+import * as cache from "./cache.js";
 
 ui.init();
-ui.setReloadCallback(loadExpenses);
-loadExpenses();
+ui.setReloadCallback(load);
+load();
 
-async function loadExpenses(event) {
+async function load(event) {
   console.log("on load expenses");
   try {
     ui.clearError();
     ui.toggleLoadingExpenses();
     ui.clearExpenses();
     cache.clearCache();
+    let friends = await model.retrieveFriends();
+    friends.map((friend) => cache.setFriend(friend));
     let expenses = await model.retrieveExpenses();
     expenses.map((expense) => ui.addExpenseItem(expense));
-    expenses.forEach(exp => cache.setExpense(exp));
+    expenses.map((expense) => cache.setExpense(expense));
     ui.toggleLoadingExpenses();
   } catch(error) {
     ui.toggleLoadingExpenses();
